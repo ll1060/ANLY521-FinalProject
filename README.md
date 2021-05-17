7daf371e737f785923875d417589c27761e7d108
Leyao Li <ll1060@georgetown.edu>
2021-05-17 19:55:02 -0400

    commit for submission

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # ANLY521-FinalProject
+
+This project aims to shed light on the business’s ratings on Yelp using user’s textual reviews
+and built classifiers to predict the star ratings. Furthermore, this project will investigate 
+the ability for each model developed to distinguish between 1-star and 5-star ratings and below 
+2-star and above 4-star ratings. 
+
+# Files
+1. yelp_review.csv: dataset used in this project.
+   
+2. yelp_1_5_star_Review_Classification.py: contains everything in classification task 1: classify 
+1-star and 5-star ratings. filter dataset so that only reviews with 1-star and 5-star are remained,
+train and test split, initialize tf-idf vectorizer, train word2vec model, calculate average of word2vec, 
+   train and test and metrics for model performance. 
+   
+example usage: python yelp_1_5_star_Review_Classification.py --yelp_review yelp_review.csv
+
+3. yelp_2_4_star_Review_Classification.py: contains everything in classification task 2: classify 
+<=2-star and >=4-star ratings. Filter dataset so that only reviews with <=2-star and >=4-star are remained,
+train and test split, initialize tf-idf vectorizer, train word2vec model, calculate average of word2vec, 
+   train and test and metrics for model performance. 
+   
+example usage: python yelp_2_4_star_Review_Classification.py --yelp_review yelp_review.csv
+
+# Requirements:
+The follow packages are needed:\
+os\
+numpy\
+pandas\
+sklearn.preprocessing\
+sklearn.preprocessing: LabelEncoder\
+sklearn.linear_model: LogisticRegression\
+sklearn.feature_extraction.text: TfidfVectorizer\
+sklearn.model_selection: train_test_split\
+smart_open\
+gensim.models:Word2Vec\
+sklearn: metrics\
+sklearn: svm\
+gensim\
+argparse\
+sklearn: naive_bayes\
---
+++ yelp_1_5_star_Review_Classification.py
@@ -0,0 +1,106 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import smart_open
+smart_open.open = smart_open.smart_open
+from gensim.models import Word2Vec
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn import naive_bayes, svm
+import gensim
+import argparse
+import sys, os
+
+
+def main(yelp_reivew):
+    #read in the dataset
+    yelp_reviews = pd.read_csv(args.yelp_review)
+
+    # filter the data to contain only 1 star and 5 star reviews
+    df_1_5_star = yelp_reviews[(yelp_reviews['stars'] == 1.0) | (yelp_reviews['stars'] == 5.0)]
+
+
+
+    # encode label for classification
+    le = preprocessing.LabelEncoder()
+    le.fit(df_1_5_star.stars)
+    df_1_5_star['label'] = le.transform(df_1_5_star.stars)
+
+    # train, test split
+    X_train, X_test, y_train, y_test = train_test_split(df_1_5_star['text'], df_1_5_star['label'],
+                                                        random_state = 0,test_size=0.25)
+
+    # initiate a tfidf vectorizer
+    tfidf_vectorizer = TfidfVectorizer(stop_words='english',
+                                       ngram_range=(1, 2),
+                                       lowercase=True,
+                                       max_features=3000)
+
+    # transform X_train and X_test
+    X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+    X_test_tfidf= tfidf_vectorizer.transform(X_test)
+
+    # train and test a naive bayes with tfidf vectors
+    nb_tfidf = naive_bayes.MultinomialNB()
+    nb_tfidf.fit(X_train_tfidf,y_train)
+    # predict the labels on test dataset
+    predictions_nb = nb_tfidf.predict(X_test_tfidf)
+    # check precision, recall, f1-score for naive bayes with tfidf
+    print("Naive Bayes with TFIDF vectors classification report:\n", metrics.classification_report(y_test,predictions_nb))
+
+    # initiate logistic regression model with tfidf vectors, fit the model to train set and test the model on test set
+    logreg_tfidf = LogisticRegression(random_state=0).fit(X_train_tfidf,y_train)
+    logreg_tfidf_pred = logreg_tfidf.predict(X_test_tfidf)
+
+    # check precision, recall, f1-score for logistic regression with tfidf
+    print("Logistic Regression with Tfidf Vectors classification report:\n",
+          metrics.classification_report(y_test, logreg_tfidf_pred))
+
+    # train and test a linear SVM with tfidf vectors
+    svm_tfidf = svm.SVC(C=1.0, kernel='linear', gamma='auto')
+    svm_tfidf.fit(X_train_tfidf,y_train)
+    # predict the labels on validation dataset
+    svm_tfidf_pred = svm_tfidf.predict(X_test_tfidf)
+    # check precision, recall, f1-score for linear SVM with tfidf
+    print("Linear SVM with TFIDF vectors classification report:\n", metrics.classification_report(y_test,svm_tfidf_pred))
+
+    # build word embedding vectors
+    # initiate a word2vec model
+    w2v = gensim.models.Word2Vec(list(df_1_5_star.text), size=200, window=10, min_count=2, iter=10)
+
+    # write a function to calculate the mean of word2vec vectors
+    def document_vector(doc):
+        """Create document vectors by averaging word vectors. Also remove out-of-vocabulary words."""
+        doc = [word for word in doc if word in w2v.wv.vocab]
+        return np.mean(w2v[doc], axis=0)
+
+    # build w2v vectors for train and test data
+    X_train['doc_vector'] = X_train.apply(document_vector)
+    X_test['doc_vector'] = X_test.apply(document_vector)
+    X_train_w2v = list(X_train['doc_vector'])
+    X_test_w2v = list(X_test['doc_vector'])
+
+    # train a logistic regression with w2v vectors
+    logreg_w2v = LogisticRegression(random_state=0).fit(X_train_w2v,y_train)
+    # make predictions using the model trained on test set
+    logreg_w2v_pred = logreg_w2v.predict(X_test_w2v)
+    print("Logistic Regression with w2v vectors classification report:\n", metrics.classification_report(y_test,logreg_w2v_pred))
+
+    # train a linear SVM with w2v vectors on the train set
+    svm_w2v = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+    svm_w2v.fit(X_train_w2v, y_train)
+    # make predictions using the trained model on test set
+    svm_w2v_pred = svm_w2v.predict(X_test_w2v)
+    print("Linear SVM with w2v classification report:\n", metrics.classification_report(y_test, svm_w2v_pred))
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--yelp_review", type=str, default="yelp_review.csv",
+                        help="yelp review file")
+    args = parser.parse_args()
+    main(args.yelp_review)
